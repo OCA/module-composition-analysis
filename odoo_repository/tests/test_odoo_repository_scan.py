3fa6573e8b6e33009e16db404a54c4f49b7f24f3
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-04-19 16:51:18 +0200

    odoo_repository: test repository scan

---
+++ odoo_repository/tests/test_odoo_repository_scan.py
@@ -0,0 +1,43 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from .common import Common
+
+
+class TestOdooRepositoryScan(Common):
+    def test_check_config(self):
+        self.odoo_repository._check_config()
+
+    def test_action_scan(self):
+        module = self.env["odoo.module"].search([("name", "=", self.module_name)])
+        self.assertFalse(module)
+        self.odoo_repository.with_context(test_queue_job_no_delay=True).action_scan(
+            [self.branch.name]
+        )
+        # Check module technical name
+        module = self.env["odoo.module"].search([("name", "=", self.module_name)])
+        self.assertTrue(module)
+        # Check module branch
+        module_branch = self.env["odoo.module.branch"].search(
+            [("module_id", "=", module.id), ("branch_id", "=", self.branch.id)]
+        )
+        self.assertEqual(module_branch.module_name, self.module_name)
+        self.assertTrue(module_branch.last_scanned_commit)
+        self.assertEqual(module_branch.repository_id, self.odoo_repository)
+        self.assertEqual(module_branch.org_id, self.org)
+        self.assertEqual(module_branch.title, "Test")
+        self.assertEqual(module_branch.category_id.name, "Test Module")
+        self.assertItemsEqual(
+            module_branch.author_ids.mapped("name"),
+            ["Odoo Community Association (OCA)", "Camptocamp"],
+        )
+        self.assertEqual(module_branch.dependency_ids.module_name, "base")
+        self.assertEqual(module_branch.license_id.name, "AGPL-3")
+        self.assertEqual(module_branch.version, "1.0.0")
+        self.assertTrue(module_branch.sloc_python)
+        # Check repository branch
+        repo_branch = module_branch.repository_branch_id
+        self.assertEqual(repo_branch.branch_id, self.branch)
+        self.assertEqual(
+            repo_branch.last_scanned_commit, module_branch.last_scanned_commit
+        )
