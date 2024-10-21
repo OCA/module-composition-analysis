OCA/module-composition-analysis

commit 646acc06bd051490a1b2850dab1c32a022084c29

BaseScanner: improve clone IO performance
The use of '--filter=blob:none' was generating more disk IO operations
than without. On a network mounted volume, the performance of any git
operations decreases a lot compared to a std clone (with all blobs).

Also, to improve the time of a clone operation, we restrict it to a
single branch. If another branch is required later, it'll be fetched
automatically anyway.

odoo_repository/lib/scanner.py
@@ -205,15 +205,13 @@ class BaseScanner:
         params = {
             "url": self.clone_url,
             "to_path": self.path,
-            # NOTE: adding 'no_checkout' and 'filter=blob:none' allows fast
-            # cloning and reduce memory usage. Blobs will be fetched later on
-            # demand, once the git config to reduce memory usage is applied.
             "no_checkout": True,
-            "filter": "blob:none",
             # Avoid issues with file permissions
             # "allow_unsafe_options": True,
             # "multi_options": ["--config core.filemode=false"],
         }
+        if self.branches:
+            params["branch"] = self.branches[0]
         params.update(extra)
         return params
 

odoo_repository/tests/test_base_scanner.py
@@ -69,7 +69,7 @@ class TestBaseScanner(Common):
         scanner = self._init_scanner()
         scanner.sync()
         with scanner.repo() as repo:
-            branch = self._settings["branch1"]
+            branch = self._settings["branch2"]
             branch_sha = repo.refs[f"origin/{branch}"].object.hexsha
             self.assertNotEqual(repo.head.object.hexsha, branch_sha)
             scanner._checkout_branch(repo, branch)

commit 8158d00ce13dcce4713189b62b15625fa7e173af

BaseScanner: do not raise if a branch is not found when cloning
Issue introduced with commit 646acc06bd051490a1b2850dab1c32a022084c29

odoo_repository/lib/scanner.py
@@ -89,11 +89,12 @@ class BaseScanner:
         # Clone or update the repository
         if not self.is_cloned:
             res = self._clone()
-        with self.repo() as repo:
-            self._apply_git_config(repo)
-            self._set_git_remote_url(repo)
-            if fetch:
-                res = self._fetch(repo)
+        if self.is_cloned:
+            with self.repo() as repo:
+                self._apply_git_config(repo)
+                self._set_git_remote_url(repo)
+                if fetch:
+                    res = self._fetch(repo)
         return res
 
     @contextlib.contextmanager
@@ -227,14 +228,25 @@ class BaseScanner:
                 if self.workaround_fs_errors:
                     extra["separate_git_dir"] = str(tmp_git_dir_path)
                 params = self._clone_params(**extra)
-                git.Repo.clone_from(**params)
-                if tmp_git_dir_path:
-                    # {repo_path}/.git folder is a hardlink, replace
-                    # it by the .git folder created in /tmp
-                    # NOTE: use shutil instead of 'pathlib.Path.replace()' as
-                    # file systems could be different
-                    repo_git_dir_path.unlink()
-                    shutil.move(tmp_git_dir_path, repo_git_dir_path)
+                try:
+                    git.Repo.clone_from(**params)
+                except git.exc.GitCommandError as exc:
+                    _logger.error(exc)
+                    if "not found in upstream origin" in str(exc):
+                        _logger.info(
+                            "Couldn't clone remote branch from %s, skipping.",
+                            self.full_name,
+                        )
+                        return False
+                else:
+                    if tmp_git_dir_path:
+                        # {repo_path}/.git folder is a hardlink, replace
+                        # it by the .git folder created in /tmp
+                        # NOTE: use shutil instead of 'pathlib.Path.replace()' as
+                        # file systems could be different
+                        if repo_git_dir_path.exists():
+                            repo_git_dir_path.unlink()
+                        shutil.move(tmp_git_dir_path, repo_git_dir_path)
         return True
 
     def _fetch(self, repo):