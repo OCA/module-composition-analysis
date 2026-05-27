9424899dbfc93b2a7bdc09c0629f07d81ffa0224
Sébastien Alix <seb@usr-src.org>
2026-05-27 14:36:18 +0200

    [FIX] odoo_repository_migration: handle modules renamed/replaced multiple times
    A module can be renamed/replaced multiple time between two Odoo
    versions, and the migration data should show the last relevant module.
    
    Also, the computation of relevant migration data is automatically triggered
    when timelines are created/updated.
    
    A convenient "Scan" button has been added on timeline form to trigger a
    scan on relevant modules if needed, as well as a "Migrations" smart
    button to access related migration data records.

--- odoo_repository_migration/tests/test_odoo_module_branch.py
+++ odoo_repository_migration/tests/test_odoo_module_branch.py
@@ -34,13 +34,7 @@ class TestOdooModuleBranch(MigrationCommon):
             last_scanned_commit="sha",
         )
         # Add timeline entry for renaming
-        self.module_branch.timeline_ids.create(
-            {
-                "module_branch_id": self.module_branch.id,
-                "state": "renamed",
-                "next_module_id": next_module.id,
-            }
-        )
+        self._create_timeline(self.module_branch, next_module, "renamed")
         # Test next_odoo_version_module_branch_id follows renaming
         self.assertEqual(
             self.module_branch.next_odoo_version_module_branch_id, next_module_branch
@@ -132,7 +126,7 @@ class TestOdooModuleBranch(MigrationCommon):
             self.module,
             self.branch2,
             specific=False,
-            repository_branch_id=self.repo_branch.id,
+            repository_branch_id=self.repo_branch2.id,
             # Module available in a PR
             pr_url="https://my/pr",
         )
@@ -166,9 +160,6 @@ class TestOdooModuleBranch(MigrationCommon):
                 # When 'pr_url' is unset, this means the module has been merged
                 "pr_url": False,
             }
-        )
-        self.module_branch.migration_ids.last_target_scanned_commit = (
-            target_module_branch.last_scanned_commit
         )
         self.assertEqual(
             self.module_branch.migration_ids.target_module_branch_id,
@@ -308,9 +299,19 @@ class TestOdooModuleBranch(MigrationCommon):
         self.assertEqual(mig.state, "moved_to_generic")
         self.assertFalse(mig.migration_scan)
 
-    def test_renamed_to_module_in_target_version(self):
+    def test_renamed_module(self):
+        """Test migration data of a module renamed.
+
+        Data input:
+            - source version = X
+            - target version = X+1
+            - module renamed in X+1
+
+        Expected ouput:
+            - 'renamed_to_module_id' should target module renamed in X+1
+        """
         self.odoo_repository.collect_migration_data = True
-        # Next version is 16.0
+        # Next version is X+1
         next_branch = self.env["odoo.branch"].search(
             [("sequence", "=", self.branch.sequence + 1)]
         )
@@ -321,10 +322,10 @@ class TestOdooModuleBranch(MigrationCommon):
             new_module,
             next_branch,
             specific=False,
-            repository_branch_id=self.repo_branch.id,
+            repository_branch_id=self.repo_branch2.id,
             last_scanned_commit="sha",
         )
-        # Generate migration data records
+        # Generate migration data records from X to X+1
         self.env["odoo.migration.path"].create(
             {
                 "source_branch_id": self.branch.id,
@@ -337,19 +338,13 @@ class TestOdooModuleBranch(MigrationCommon):
             target=next_branch,
             report={"process": "migrate", "results": {}},
         )
-        # Module has been renamed starting from 16.0
-        self.module_branch.timeline_ids.create(
-            {
-                "module_branch_id": self.module_branch.id,
-                "state": "renamed",
-                "next_module_id": new_module.id,
-            }
-        )
+        # Module has been renamed starting from X+1
+        self._create_timeline(self.module_branch, new_module, "renamed")
         renamed_to_module = self.module_branch._renamed_to_module_in_target_version(
             next_branch
         )
         self.assertEqual(renamed_to_module, new_module)
-        # We target 17.0 to check if intermediate data in 16.0 is found
+        # We target X+2 to check if intermediate data in X+1 is found
         target_branch = self.env["odoo.branch"].search(
             [("sequence", "=", self.branch.sequence + 2)]
         )
@@ -363,12 +358,97 @@ class TestOdooModuleBranch(MigrationCommon):
         self.assertFalse(mig.replaced_by_module_id)
         self.assertEqual(mig.target_module_branch_id, target_module_branch)
         self.assertFalse(mig.last_target_scanned_commit)
-        self.assertEqual(mig.state, "migrate")
+        self.assertEqual(mig.state, "fully_ported")
         self.assertTrue(mig.migration_scan)
 
-    def test_replaced_by_module_in_target_version(self):
+    def test_renamed_module_twice(self):
+        """Test migration data of a module renamed twice.
+
+        Data input:
+            - source version = X
+            - target version = X+2
+            - module renamed in X+1
+            - module renamed again in X+2
+
+        Expected ouput:
+            - 'renamed_to_module_id' should target module renamed in X+2
+        """
         self.odoo_repository.collect_migration_data = True
-        # Next version is 16.0
+        # Next version is X+1
+        next_branch = self.env["odoo.branch"].search(
+            [("sequence", "=", self.branch.sequence + 1)]
+        )
+        self.assertEqual(self.branch.next_id, next_branch)
+        # Target version is X+2
+        target_branch = self.env["odoo.branch"].search(
+            [("sequence", "=", self.branch.sequence + 2)]
+        )
+        self.assertEqual(self.branch.next_id.next_id, target_branch)
+        # Create the next module
+        next_module = self.module.copy({"name": "next_module"})
+        next_module_branch = self._create_odoo_module_branch(
+            next_module,
+            next_branch,
+            specific=False,
+            repository_branch_id=self.repo_branch2.id,
+            last_scanned_commit="sha_next",
+        )
+        # Create the target module
+        target_module = self.module.copy({"name": "target_module"})
+        target_module_branch = self._create_odoo_module_branch(
+            target_module,
+            target_branch,
+            specific=False,
+            repository_branch_id=self.repo_branch3.id,
+            last_scanned_commit="sha_target",
+        )
+        # Generate migration data records from X to X+2 (with a gap)
+        self.env["odoo.migration.path"].create(
+            {
+                "source_branch_id": self.branch.id,
+                "target_branch_id": target_branch.id,
+            }
+        )
+        self._simulate_migration_scan(
+            "target_commit1",
+            source=self.branch,
+            target=target_branch,
+            report={"process": "migrate", "results": {}},
+        )
+        # Module has been renamed in X+1
+        self._create_timeline(self.module_branch, next_module, "renamed")
+        renamed_to_module = self.module_branch._renamed_to_module_in_target_version(
+            next_branch
+        )
+        self.assertEqual(renamed_to_module, next_module)
+        # Module has been renamed again in X+2
+        self._create_timeline(next_module_branch, target_module, "renamed")
+        renamed_to_module = next_module_branch._renamed_to_module_in_target_version(
+            target_branch
+        )
+        self.assertEqual(renamed_to_module, target_module)
+        # Check migration data
+        mig = self.module_branch.migration_ids
+        self.assertEqual(mig.renamed_to_module_id, target_module)
+        self.assertFalse(mig.replaced_by_module_id)
+        self.assertEqual(mig.target_module_branch_id, target_module_branch)
+        self.assertFalse(mig.last_target_scanned_commit)
+        self.assertEqual(mig.state, "fully_ported")
+        self.assertTrue(mig.migration_scan)
+
+    def test_replaced_module(self):
+        """Test migration data of a module replaced.
+
+        Data input:
+            - source version = X
+            - target version = X+1
+            - module replaced in X+1
+
+        Expected ouput:
+            - 'replaced_by_module_id' should target module replaced in X+1
+        """
+        self.odoo_repository.collect_migration_data = True
+        # Next version is X+1
         next_branch = self.env["odoo.branch"].search(
             [("sequence", "=", self.branch.sequence + 1)]
         )
@@ -395,19 +475,13 @@ class TestOdooModuleBranch(MigrationCommon):
             target=next_branch,
             report={"process": "migrate", "results": {}},
         )
-        # New module is replacing current one starting from 16.0
-        self.module_branch.timeline_ids.create(
-            {
-                "module_branch_id": self.module_branch.id,
-                "state": "replaced",
-                "next_module_id": new_module.id,
-            }
-        )
+        # New module is replacing current one starting from X+1
+        self._create_timeline(self.module_branch, new_module, "replaced")
         replaced_by_module = self.module_branch._replaced_by_module_in_target_version(
             next_branch
         )
         self.assertEqual(replaced_by_module, new_module)
-        # We target 17.0 to check if intermediate data in 16.0 is found
+        # We target X+2 to check if intermediate data in X+1 is found
         target_branch = self.env["odoo.branch"].search(
             [("sequence", "=", self.branch.sequence + 2)]
         )
@@ -419,6 +493,235 @@ class TestOdooModuleBranch(MigrationCommon):
         mig = self.module_branch.migration_ids
         self.assertEqual(mig.replaced_by_module_id, new_module)
         self.assertFalse(mig.renamed_to_module_id)
+        self.assertEqual(mig.target_module_branch_id, target_module_branch)
+        self.assertFalse(mig.last_target_scanned_commit)
+        self.assertEqual(mig.state, "replaced")
+        # No migration scan needed for replaced modules
+        self.assertFalse(mig.migration_scan)
+
+    def test_replaced_module_twice(self):
+        """Test migration data of a module replaced twice.
+
+        Data input:
+            - source version = X
+            - target version = X+2
+            - module replaced in X+1
+            - module replaced again in X+2
+
+        Expected ouput:
+            - 'replaced_by_module_id' should target module replaced in X+2
+        """
+        self.odoo_repository.collect_migration_data = True
+        # Next version is X+1
+        next_branch = self.env["odoo.branch"].search(
+            [("sequence", "=", self.branch.sequence + 1)]
+        )
+        self.assertEqual(self.branch.next_id, next_branch)
+        # Target version is X+2
+        target_branch = self.env["odoo.branch"].search(
+            [("sequence", "=", self.branch.sequence + 2)]
+        )
+        self.assertEqual(self.branch.next_id.next_id, target_branch)
+        # Create the next module
+        next_module = self.module.copy({"name": "next_module"})
+        next_module_branch = self._create_odoo_module_branch(
+            next_module,
+            next_branch,
+            specific=False,
+            repository_branch_id=self.repo_branch2.id,
+            last_scanned_commit="sha_next",
+        )
+        # Create the target module
+        target_module = self.module.copy({"name": "target_module"})
+        target_module_branch = self._create_odoo_module_branch(
+            target_module,
+            target_branch,
+            specific=False,
+            repository_branch_id=self.repo_branch3.id,
+            last_scanned_commit="sha_target",
+        )
+        # Generate migration data records from X to X+2 (with a gap)
+        self.env["odoo.migration.path"].create(
+            {
+                "source_branch_id": self.branch.id,
+                "target_branch_id": target_branch.id,
+            }
+        )
+        self._simulate_migration_scan(
+            "target_commit1",
+            source=self.branch,
+            target=target_branch,
+            report={"process": "migrate", "results": {}},
+        )
+        # Module has been replaced in X+1
+        self._create_timeline(self.module_branch, next_module, "replaced")
+        replaced_by_module = self.module_branch._replaced_by_module_in_target_version(
+            next_branch
+        )
+        self.assertEqual(replaced_by_module, next_module)
+        # Module has been replaced again in X+2
+        self._create_timeline(next_module_branch, target_module, "replaced")
+        replaced_by_module = next_module_branch._replaced_by_module_in_target_version(
+            target_branch
+        )
+        self.assertEqual(replaced_by_module, target_module)
+        # Check migration data
+        mig = self.module_branch.migration_ids
+        self.assertEqual(mig.replaced_by_module_id, target_module)
+        self.assertFalse(mig.renamed_to_module_id)
+        self.assertEqual(mig.target_module_branch_id, target_module_branch)
+        self.assertFalse(mig.last_target_scanned_commit)
+        self.assertEqual(mig.state, "replaced")
+        # No migration scan needed for replaced modules
+        self.assertFalse(mig.migration_scan)
+
+    def test_renamed_then_replaced_module(self):
+        """Test migration data of a module renamed then replaced.
+
+        Data input:
+            - source version = X
+            - target version = X+2
+            - module renamed in X+1
+            - module replaced in X+2
+
+        Expected ouput:
+            - 'renamed_to_module_id' should be empty
+            - 'replaced_by_module_id' should target module replaced in X+2
+        """
+        self.odoo_repository.collect_migration_data = True
+        # Next version is X+1
+        next_branch = self.env["odoo.branch"].search(
+            [("sequence", "=", self.branch.sequence + 1)]
+        )
+        self.assertEqual(self.branch.next_id, next_branch)
+        # Target version is X+2
+        target_branch = self.env["odoo.branch"].search(
+            [("sequence", "=", self.branch.sequence + 2)]
+        )
+        self.assertEqual(self.branch.next_id.next_id, target_branch)
+        # Create the next module
+        next_module = self.module.copy({"name": "next_module"})
+        next_module_branch = self._create_odoo_module_branch(
+            next_module,
+            next_branch,
+            specific=False,
+            repository_branch_id=self.repo_branch2.id,
+            last_scanned_commit="sha_next",
+        )
+        # Create the target module
+        target_module = self.module.copy({"name": "target_module"})
+        target_module_branch = self._create_odoo_module_branch(
+            target_module,
+            target_branch,
+            specific=False,
+            repository_branch_id=self.repo_branch3.id,
+            last_scanned_commit="sha_target",
+        )
+        # Generate migration data records from X to X+2 (with a gap)
+        self.env["odoo.migration.path"].create(
+            {
+                "source_branch_id": self.branch.id,
+                "target_branch_id": target_branch.id,
+            }
+        )
+        self._simulate_migration_scan(
+            "target_commit1",
+            source=self.branch,
+            target=target_branch,
+            report={"process": "migrate", "results": {}},
+        )
+        # Module has been renamed in X+1
+        self._create_timeline(self.module_branch, next_module, "renamed")
+        renamed_to_module = self.module_branch._renamed_to_module_in_target_version(
+            next_branch
+        )
+        self.assertEqual(renamed_to_module, next_module)
+        # Module has been replaced in X+2
+        self._create_timeline(next_module_branch, target_module, "replaced")
+        replaced_by_module = next_module_branch._replaced_by_module_in_target_version(
+            target_branch
+        )
+        self.assertEqual(replaced_by_module, target_module)
+        # Check migration data
+        mig = self.module_branch.migration_ids
+        self.assertFalse(mig.renamed_to_module_id)
+        self.assertEqual(mig.replaced_by_module_id, target_module)
+        self.assertEqual(mig.target_module_branch_id, target_module_branch)
+        self.assertFalse(mig.last_target_scanned_commit)
+        self.assertEqual(mig.state, "replaced")
+        self.assertFalse(mig.migration_scan)
+
+    def test_replaced_then_renamed_module(self):
+        """Test migration data of a module replaced then renamed.
+
+        Data input:
+            - source version = X
+            - target version = X+2
+            - module replaced in X+1
+            - module renamed in X+2
+
+        Expected ouput:
+            - 'renamed_to_module_id' should be empty
+            - 'replaced_by_module_id' should target module renamed in X+2
+        """
+        self.odoo_repository.collect_migration_data = True
+        # Next version is X+1
+        next_branch = self.env["odoo.branch"].search(
+            [("sequence", "=", self.branch.sequence + 1)]
+        )
+        self.assertEqual(self.branch.next_id, next_branch)
+        # Target version is X+2
+        target_branch = self.env["odoo.branch"].search(
+            [("sequence", "=", self.branch.sequence + 2)]
+        )
+        self.assertEqual(self.branch.next_id.next_id, target_branch)
+        # Create the next module
+        next_module = self.module.copy({"name": "next_module"})
+        next_module_branch = self._create_odoo_module_branch(
+            next_module,
+            next_branch,
+            specific=False,
+            repository_branch_id=self.repo_branch2.id,
+            last_scanned_commit="sha_next",
+        )
+        # Create the target module
+        target_module = self.module.copy({"name": "target_module"})
+        target_module_branch = self._create_odoo_module_branch(
+            target_module,
+            target_branch,
+            specific=False,
+            repository_branch_id=self.repo_branch3.id,
+            last_scanned_commit="sha_target",
+        )
+        # Generate migration data records from X to X+2 (with a gap)
+        self.env["odoo.migration.path"].create(
+            {
+                "source_branch_id": self.branch.id,
+                "target_branch_id": target_branch.id,
+            }
+        )
+        self._simulate_migration_scan(
+            "target_commit1",
+            source=self.branch,
+            target=target_branch,
+            report={"process": "migrate", "results": {}},
+        )
+        # Module has been replaced in X+1
+        self._create_timeline(self.module_branch, next_module, "replaced")
+        replaced_by_module = self.module_branch._replaced_by_module_in_target_version(
+            next_branch
+        )
+        self.assertEqual(replaced_by_module, next_module)
+        # Module has been renamed in X+2
+        self._create_timeline(next_module_branch, target_module, "renamed")
+        renamed_to_module = next_module_branch._renamed_to_module_in_target_version(
+            target_branch
+        )
+        self.assertEqual(renamed_to_module, target_module)
+        # Check migration data
+        mig = self.module_branch.migration_ids
+        self.assertFalse(mig.renamed_to_module_id)
+        self.assertEqual(mig.replaced_by_module_id, target_module)
         self.assertEqual(mig.target_module_branch_id, target_module_branch)
         self.assertFalse(mig.last_target_scanned_commit)
         self.assertEqual(mig.state, "replaced")
