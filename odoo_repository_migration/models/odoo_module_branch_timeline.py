6b3201abba1542dba20808e1422098ed3719dd73
Sébastien Alix <seb@usr-src.org>
2026-06-09 18:57:59 +0200

    [IMP] odoo_repository_migration: add helper to get all related timelines recursively

--- odoo_repository_migration/models/odoo_module_branch_timeline.py
+++ odoo_repository_migration/models/odoo_module_branch_timeline.py
@@ -100,3 +100,48 @@ class OdooModuleBranchTimeline(models.Model):
             migrations._compute_renamed_to_module_id()
             migrations._compute_replaced_by_module_id()
             migrations._compute_state()
+
+    def _get_related_timelines(self):
+        """Return timelines related to current ones regarding impacted modules.
+
+        A module A could be renamed to a module B in version X, then renamed
+        again to C in version X+1, etc.
+        When calling this method on any timeline, the others are returned.
+        """
+        impacted_modules = self._get_all_impacted_modules()
+        return self.search(
+            [
+                "|",
+                ("module_branch_id.module_id", "in", impacted_modules.ids),
+                ("next_module_id", "in", impacted_modules.ids),
+            ]
+        )
+
+    # TODO: add ormcache?
+    def _get_all_impacted_modules(self, visited=None):
+        """Recursively collect all modules involved in the chain of timelines."""
+        if visited is None:
+            visited = self.env["odoo.module.branch.timeline"].browse()
+        impacted_modules = self.env["odoo.module"].browse()
+        for rec in self:
+            if rec in visited:
+                continue
+            visited |= rec
+            impacted_modules |= rec.module_branch_id.module_id + rec.next_module_id
+            # Find previous timelines (current module is next_module_id)
+            previous_timelines = self.search(
+                [
+                    ("next_module_id", "=", rec.module_branch_id.module_id.id),
+                ]
+            )
+            for timeline in previous_timelines:
+                impacted_modules |= timeline._get_all_impacted_modules(visited)
+            # Find next timelines (next_module_id is the module_branch_id)
+            next_timelines = self.search(
+                [
+                    ("module_branch_id.module_id", "=", rec.next_module_id.id),
+                ]
+            )
+            for timeline in next_timelines:
+                impacted_modules |= timeline._get_all_impacted_modules(visited)
+        return impacted_modules
